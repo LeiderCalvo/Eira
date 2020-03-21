2ea31467074340748984201595a08c9a041107a2
LeiderCalvo <leiderstivencalvogomez@hotmail.com>
2020-03-21 13:18:19 -0500

    Updates

---
+++ precache-manifest.e050df6192d5421b43e8b94a0abfebd9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9518c13bcc3bff322ab98c16062e257",
+    "url": "/Eira/index.html"
+  },
+  {
+    "revision": "e80db0b097cf66d4a068",
+    "url": "/Eira/static/css/main.98c5ffd4.chunk.css"
+  },
+  {
+    "revision": "cdddc09ed44b530238fe",
+    "url": "/Eira/static/js/2.75c8b918.chunk.js"
+  },
+  {
+    "revision": "e80db0b097cf66d4a068",
+    "url": "/Eira/static/js/main.736143be.chunk.js"
+  },
+  {
+    "revision": "ab3d14cb94420631c700",
+    "url": "/Eira/static/js/runtime-main.98f021d8.js"
+  }
+]);
