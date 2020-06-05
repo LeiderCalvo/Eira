2f1170944f8bd126d15ceb326b6c20542b79f5c1
LeiderCalvo <leiderstivencalvogomez@hotmail.com>
2020-06-05 10:01:36 -0500

    uploading credentials to make it work on github pages

---
+++ utils/firebaseCredentials.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyASEo4I6Ws-f10I5QRuE2Y0GgDgIH6HIeQ",
+    authDomain: "eira-d9017.firebaseapp.com",
+    databaseURL: "https://eira-d9017.firebaseio.com",
+    projectId: "eira-d9017",
+    storageBucket: "eira-d9017.appspot.com",
+    messagingSenderId: "1011002381494",
+    appId: "1:1011002381494:web:94cdb76012511031604217",
+    measurementId: "G-87827S2HYS"
+};
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
